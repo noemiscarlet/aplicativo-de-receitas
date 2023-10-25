8da6e9f4bf0e068c786cff17c1770e428196febb
Noemi souza <noemigiseleee@gmail.com>
2023-10-25 16:33:03 -0300

    resolvendo conflitos

--- src/Componentes/Footer/Footer.tsx
+++ src/Componentes/Footer/Footer.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './footer.css';
 
 export function Footer() {
   return (
-    <footer>
-      <Link to="/drinks">
-        <button data-testid="drinks-bottom-btn">
-          <img
-            src="../../../src/images/drinkIcon.svg"
-            alt="Botão de drinks, responsavel por te levar até a aba de drinks"
-          />
-        </button>
-      </Link>
-      <Link to="/meals">
-        <button data-testid="meals-bottom-btn">
-          <img
-            src="../../../src/images/mealIcon.svg"
-            alt="Botão de comidas, responsavel por te levar até a aba de drinks"
-          />
-        </button>
-      </Link>
+    <footer className="footer" data-testid="footer">
+      <NavLink to="/drinks">
+        <img
+          src="../../../src/images/drinkIcon.svg"
+          alt="Botão de drinks, responsavel por te levar até a aba de drinks"
+          data-testid="drinks-bottom-btn"
+        />
+      </NavLink>
+      <NavLink to="/meals">
+        <img
+          src="../../../src/images/mealIcon.svg"
+          alt="Botão de comidas, responsavel por te levar até a aba de drinks"
+          data-testid="meals-bottom-btn"
+        />
+      </NavLink>
     </footer>
   );
 }

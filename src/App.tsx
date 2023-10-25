1347d83001de6e3b8a3513a04993020d6fc74ff3
Stephany <stephykpcarmo@outlook.com>
2023-10-25 15:39:08 -0300

    feat: Req 07-09 finalizados com testes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,30 @@ import Login from './pages/Login/Login';
 import Recipes from './pages/Recipes/Recipes';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './componentes/Layout';
+import Profile from './pages/Profile/Profile';
+import Drinks from './pages/Drinks/Drinks';
+import DoneRecipes from './pages/Recipes/DoneRecipes';
+import FavoriteRecipes from './pages/Recipes/FavoriteRecipes';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="recipesAppMain">
       <Routes>
         <Route path="/" element={ <Login /> } />
-        <Route path="/meals" element={ <Recipes /> } />
+        <Route element={ <Layout /> }>
+          <Route path="/meals" element={ <Recipes /> } />
+          <Route path="/drinks" element={ <Drinks /> } />
+          <Route path="/profile" element={ <Profile /> } />
+          <Route path="/done-recipes" element={ <DoneRecipes /> } />
+          <Route path="favorite-recipes" element={ <FavoriteRecipes /> } />
+          <Route path="/*" element={ <NotFound /> } />
+        </Route>
+        <Route path="/meals/:id-da-receita" element={ <Recipes /> } />
+        <Route path="/meals/:id-da-receita/in-progress" element={ <Recipes /> } />
+        <Route path="/drinks/:id-da-receita" element={ <Drinks /> } />
+        <Route path="/drinks/:id-da-receita/in-progress" element={ <Drinks /> } />
       </Routes>
     </div>
   );
---
+++ src/pages/Recipes/DoneRecipes.tsx
@@ -0,0 +1,7 @@
+function DoneRecipes() {
+  return (
+    <p>Done Recipes</p>
+  );
+}
+
+export default DoneRecipes;

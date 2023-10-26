0529d63c4a8afef14ba3767f9408bfb4bd164082
VTZBBBUDI <artyarthur3@gmail.com>
2023-10-26 20:04:32 -0300

    finalmente-parte2-terminanmos-falta-os-teste-e-o-requisito-14

--- src/pages/Recipes/Recipes.tsx
+++ src/pages/Recipes/Recipes.tsx
@@ -5,18 +5,20 @@ import { Footer } from '../../componentes/Footer/Footer';
 export default function Meals() {
   const { resultsMealSearch } = useContext(SearchContext);
   return (
-    <ul>
-      {resultsMealSearch?.map((infoSearch) => (
-        <li key={ infoSearch.idMeal }>
-          <img
-            src={ infoSearch.strMealThumb }
-            alt={ infoSearch.strMeal }
-            style={ { width: '150px' } }
-          />
-          {infoSearch.strMeal}
-        </li>
-      ))}
-    </ul>
-    <Footer />
+    <>
+      <ul>
+        {resultsMealSearch?.map((infoSearch) => (
+          <li key={ infoSearch.idMeal }>
+            <img
+              src={ infoSearch.strMealThumb }
+              alt={ infoSearch.strMeal }
+              style={ { width: '150px' } }
+            />
+            {infoSearch.strMeal}
+          </li>
+        ))}
+      </ul>
+      <Footer />
+    </>
   );
 }
